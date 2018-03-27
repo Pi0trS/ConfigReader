1447021f690b24a7ac7032f84c9660ca8ae57c79
Pi0trS <piotrsy82@gmail.com>
2018-03-27 16:51:15 +0200

    add load config from stream

--- ConfigReader/Source.cpp
+++ ConfigReader/Source.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
+#include <string>
+
 #include"ConfigReader.h"
 using namespace std;
 int main()
 {
 	int i;
-	
+	std::string s;
 	//ConfigReader a("ConfigData.txt");
 
 
-	ConfigReader a(std::cin );
+	ConfigReader a((std::getline(std::cin,s)));
+	
 	a.checkData();
 	cin >>  i;
 	return 0;
